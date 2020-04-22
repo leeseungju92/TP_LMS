29446776ddb61d84868c5e5b9bcea838a2e8753d
leeseungju92 <58206083+leeseungju92@users.noreply.github.com>
2020-04-22 11:28:14 +0900

    Add files via upload

---
+++ teamlms2/src/main/java/com/teamlms/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.teamlms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+	@RequestMapping("/")
+	public String indexView(Model model) {
+		log.info(">>>>> INDEX PAGE VIEW");
+		
+		return "index";
+	}
+}
